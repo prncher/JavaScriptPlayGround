eedba21f6081925319fa2ae1f82b8237ee431d8a
prncher <prncher@gmail.com>
2019-01-12 20:18:44 -0800

    Add some strict uses

--- index.js
+++ index.js
@@ -42,12 +42,40 @@ function fnCaller(fn, arg) {
     }
 }
 
+function liberalFn() {
+    var obj = {};
+    Object.defineProperty(obj, 'x', { value: 5, writable: false });
+    obj.x = 6;
+
+    Object.preventExtensions(obj);
+    obj.y = 6;
+}
+
+function strictFn() {
+    'use strict';
+
+    var obj = {};
+    Object.defineProperty(obj, 'x', { value: 5, writable: false });
+    try {
+        obj.x = 6;
+    } catch (error) {
+        console.log(error);
+    }
+
+    Object.preventExtensions(obj);
+    try {
+        obj.y = 6;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 (function () {
     // Show multiple ways to call functions.
     const instance1 = new derived();
     fnCaller(instance1.function1, 'Testing 1');
     fnCaller(instance1.function2, 'Testing 2');
-    fnCaller(instance1.prototype.function2,'Testing 3');
+    fnCaller(instance1.prototype.function2, 'Testing 3');
     fnCaller(instance1.function3, 'Testing 4');
     if (derived.function3 !== undefined) {
         derived.function3('Testing 5');
@@ -61,6 +89,10 @@ function fnCaller(fn, arg) {
         derivedUsingPrototype.function3('Testing 9');
     }
 
-    fnCaller(base.function3,'Testing 10');
-    fnCaller(base.prototype.function2,'Testing 11');
+    fnCaller(base.function3, 'Testing 10');
+    fnCaller(base.prototype.function2, 'Testing 11');
+
+    // Show strict and non-strict cases
+    const liberal = new liberalFn();
+    const strict = new strictFn();
 })();
